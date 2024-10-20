66f16d8a0ae044483119e99b19f305185148ef71
Shreyash Gajanan Jadhav <sj2454925@gmail.com>
2024-10-20 11:52:53 +0530

    Dfdffd

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -84,15 +84,7 @@ function Table() {
       element.setAttribute("data-theme", "retro");
       element.style.filter = "blur(3px)";
       element.disabled = true;
-      element.classList.add(
-        "animate-jump animate-once animate-ease-in-out animate-fill-both"
-      );
-
-      setTimeout(() => {
-        element.classList.remove(
-          "animate-jump animate-once animate-ease-in-out animate-fill-both"
-        );
-      }, 1000);
+      element.classList.add("animate-jump animate-once");
     }
   }, [message]);
 
